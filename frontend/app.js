2ce546763ae2e6280aad1b49225af49aa243cb4f
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2021-05-15 17:21:36 +0200

    added loading div

--- frontend/app.js
+++ frontend/app.js
@@ -1,23 +1,30 @@
 const imagesDiv = document.querySelector(".images");
+const loadingDiv = document.querySelector(".loading");
 addImages();
 
 async function addImages() {
-  
-  
+  loadingDiv.classList.add("active");
   await fetch("http://localhost:8050/api/photos/1")
   .then(res => res.json())
   .then(res => {
     res.photos.photo.forEach(photo => {
-      console.log(photo);
-      const newDiv = document.createElement("div");
-      newDiv.classList.add("photo");
+      imagesDiv.appendChild(createPhotoBox(photo));
+    });
+    loadingDiv.classList.remove("active");
+  })
+  .catch(() => {
+    loadingDiv.innerText = "Loading failed!"
+  });
+}
+
+function createPhotoBox(photo) {
+  const photoBox = document.createElement("div");
+      photoBox.classList.add("photo");
       const image = document.createElement("img");
       image.src = `https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg`;
-      newDiv.appendChild(image);
-      const photoBox = document.createElement("h5");
-      photoBox.innerText = photo.title;
-      newDiv.appendChild(photoBox);
-      imagesDiv.appendChild(newDiv);
-    });
-  });
+      photoBox.appendChild(image);
+      const photoTitle = document.createElement("h5");
+      photoTitle.innerText = photo.title;
+      photoBox.appendChild(photoTitle);
+      return photoBox;
 }
